45ae158cd93d88a228aa3b9813b2a2885b641eee
Trouni Tiet <trouni@gmail.com>
2021-06-28 17:27:38 +0900

    Add fontawesome tailwind nprogress packages

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// FontAwesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faPhone);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
