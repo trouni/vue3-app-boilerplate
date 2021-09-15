fcae1336baa7a40cc67f88119029c92b3d1233e8
Trouni Tiet <trouni@gmail.com>
2021-09-15 16:19:49 +0900

    fix: Clean up Tailwind config

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,6 @@ export default {
 @import './assets/tailwind.css';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
